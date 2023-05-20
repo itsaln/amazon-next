fed13283ae54998dc06c24ab51bea387c09e8205
itsaln <itsaln23@gmail.com>
2023-05-20 22:18:26 +0500

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,66 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
+const primary = '#E30B13'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./app/components/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		colors: {
+			primary,
+			black: colors.black,
+			white: colors.white
+		},
+		extend: {
+			fontSize: {
+
+			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3'
+			}
+		}
+	},
+	plugins: [
+		plugin(({ addComponents, theme, addUtilities }) => {
+			addComponents({
+				'.truncate-1': {
+					display: '-webkit-box !important',
+					'-webkit-line-clamp': '1',
+					'-webkit-box-orient': 'vertical',
+					textOverflow: 'ellipsis',
+					overflow: 'hidden'
+				},
+				'.truncate-2': {
+					display: '-webkit-box !important',
+					'-webkit-line-clamp': '2',
+					'-webkit-box-orient': 'vertical',
+					textOverflow: 'ellipsis',
+					overflow: 'hidden'
+				},
+				'.truncate-3': {
+					display: '-webkit-box !important',
+					'-webkit-line-clamp': '3',
+					'-webkit-box-orient': 'vertical',
+					textOverflow: 'ellipsis',
+					overflow: 'hidden'
+				},
+				'.truncate-4': {
+					display: '-webkit-box !important',
+					'-webkit-line-clamp': '4',
+					'-webkit-box-orient': 'vertical',
+					textOverflow: 'ellipsis',
+					overflow: 'hidden'
+				}
+			})
+			addUtilities({
+
+			})
+		})
+	]
 }
